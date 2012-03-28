5f1a2cdff2619f2f32b5c50e790ad59c5a31e440
Manuel de la Peña <manuel.delapenya@liferay.com>
2012-03-28 12:55:00 +0200

    LPS-26390 DateUtilTest.java

--- portal-service/test/unit/com/liferay/portal/kernel/util/DateUtilTest.java
+++ portal-service/test/unit/com/liferay/portal/kernel/util/DateUtilTest.java
@@ -14,19 +14,83 @@
 
 package com.liferay.portal.kernel.util;
 
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.modules.junit4.PowerMockRunner;
-
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.Locale;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Alexander Chow
  * @author Manuel de la Peña
  * @author Ray Augé
  */
+@PrepareForTest(DateFormatFactoryUtil.class)
 @RunWith(PowerMockRunner.class)
 public class DateUtilTest extends PowerMockito {
 
+	@Test
+	public void testGetISOFormatLength8() {
+		_testGetISOFormat("01234567","yyyyMMdd");
+	}
+
+	@Test
+	public void testGetISOFormatLength12() {
+		_testGetISOFormat("012345678901","yyyyMMddHHmm");
+	}
+
+	@Test
+	public void testGetISOFormatLength13() {
+		_testGetISOFormat("0123456789012","yyyyMMdd'T'HHmm");
+	}
+
+	@Test
+	public void testGetISOFormatLength14() {
+		_testGetISOFormat("01234567890123","yyyyMMddHHmmss");
+	}
+
+	@Test
+	public void testGetISOFormatLength15() {
+		_testGetISOFormat("012345678901234","yyyyMMdd'T'HHmmss");
+	}
+
+	@Test
+	public void testGetISOFormatT() {
+		_testGetISOFormat("01234567T9012345","yyyyMMdd'T'HHmmssz");
+	}
+
+	@Test
+	public void testGetISOFormatAny() {
+		_testGetISOFormat(Mockito.anyString(),"yyyyMMddHHmmssz");
+	}
+
+	private void _mockDateUtilPattern(String pattern) {
+		mockStatic(DateFormatFactoryUtil.class);
+
+		when(
+			DateFormatFactoryUtil.getSimpleDateFormat(pattern)
+		).thenReturn(
+			new SimpleDateFormat(pattern, new Locale("es_ES"))
+		);
+	}
+
+	private void _testGetISOFormat(String text, String pattern) {
+		_mockDateUtilPattern(pattern);
+
+		DateFormat dateFormat = DateUtil.getISOFormat(text);
+
+		String actualPattern = ((SimpleDateFormat)dateFormat).toPattern();
+
+		Assert.assertEquals(pattern, actualPattern);
+	}
 
 }
