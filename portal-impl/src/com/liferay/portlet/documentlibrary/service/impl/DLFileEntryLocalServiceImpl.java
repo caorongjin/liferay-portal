72a5328cd40cb69a1fcaa2ba01f5f5f42a783396
Dennis Ju <dennis.ju@liferay.com>
2012-07-09 15:36:21 -0700

    LPS-28412 Updating files w/o setting original document type causes metadata to be lost

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -1191,6 +1191,10 @@ public class DLFileEntryLocalServiceImpl
 
 		String extraSettings = StringPool.BLANK;
 
+		if (fileEntryTypeId == 0) {
+			fileEntryTypeId = dlFileEntry.getFileEntryTypeId();
+		}
+
 		fileEntryTypeId = getFileEntryTypeId(
 			DLUtil.getGroupIds(dlFileEntry.getGroupId()),
 			dlFileEntry.getFolderId(), fileEntryTypeId);
